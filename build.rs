e53c414a4da928cdff9a8d4f999f3f3c4bd4bcec
Nulldata <thebreadcat@gmail.com>
2014-11-25 09:01:30 +0100

    Fixed compilation2

--- build.rs
+++ build.rs
@@ -5,13 +5,14 @@ use std::os;
 fn main() {
   let out_dir = os::getenv( "OUT_DIR" ).unwrap();
 
-  Command::new( "g++" ).args( &[ "src/api_helper.cpp"
-                               , "-c", "-std=c++11", "-m64", "-fPIC", "-o" ] )
+  Command::new( "g++" ).args( &[ "src/consoleinterface.cpp"
+                               , "-c", "-std=c++11", "-m64"
+                               , "-fPIC", "-o" ] )
                      .arg( format!( "{}/consoleinterface.o", out_dir ) )
                      .status().unwrap();
   Command::new( "ar" ).args( &[ "crus", "libconsoleinterface.a"
                               , "consoleinterface.o" ] )
                     .cwd( &Path::new( &out_dir ) )
                     .status().unwrap();
-  println!( "cargo:rustc-flags=-L {} -l consoleinterface:static", out_dir );
+  println!( "cargo:rustc-flags=-L {} -l consoleinterface:static -l stdc++:static", out_dir );
 }
