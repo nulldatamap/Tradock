9c6bc23d3457177fbe6e93d82c4555caf0c32f41
TheBreadCat <TheBreadCat@gmail.com>
2014-12-03 23:43:42 +0100

    Added pretty rendering and fixed some small interface problems

--- src/consoleinterface.rs
+++ src/consoleinterface.rs
@@ -1,9 +1,13 @@
 use std::io::stdio::{StdReader, stdin};
 use std::io::BufferedReader;
 use std::ascii::AsciiExt;
+use std::cmp::max;
+use std::num::Float;
 
-use market::Failure::{InsufficientAgentFunds, InsufficientMarketAssets, InsufficientAgentAssets};
-use market::{Count, Failure, Market};
+use market::Failure::{InsufficientAgentFunds
+                    , InsufficientMarketAssets
+                    , InsufficientAgentAssets};
+use market::{Money, Count, Failure, Market};
 use market_data::MarketData;
 use agent::Agent;
 use circularbuf::CircularBuf;
@@ -13,16 +17,90 @@ pub struct ConsoleInterface {
   input : BufferedReader<StdReader>
 }
 
+// The maximum width of the embedded graph
+fn graph_max_width() -> uint {
+  69
+}
+
+// The height of the embedded graph
+fn graph_height() -> uint {
+  12
+}
+
 fn render_market_data( player : &Agent, market : &mut Market ) {
-  println!( "Stats for: {}", market.name );
+  let price_history = &market.data.price_history;
+
+  println!( "Statistics for: {}", market.name );
   println!( "Day: {}", market.data.day_count );
-  println!( "Price per. asset: {:.2} DKK", market.price );
+
+  // How much has the market grown?
+  let growth = if price_history.len() == 1 {
+    // We haven't grown if it's the first day
+    0.
+  } else {
+    // Get the previous price
+    let previous_price = price_history[price_history.len() - 2];
+    // Calculate the price in %
+    (market.price - previous_price) / previous_price * 100.
+  };
+  println!( "Price per. asset: {:.2} DKK ( {:+.2}% growth )", market.price
+                                                            , growth );
+
   let total_price = market.assets as f64 * market.price;
   println!( "Total market value ( {} assets ): {:.2} DKK", market.assets
                                                          , total_price );
   // Find the closest disable price point:
-  
-  
+
+  println!( "\n       Price\n         ^" );
+  // Find out how many element we are skipping to fit the graph
+  // into the limited terminal window.
+  let skip_amount;
+  let graph_width;
+  // How wide does the graph need to be, and do we need to skip any data in the
+  // buffer in order to see the newewst data?
+  if price_history.len() > graph_max_width() {
+    skip_amount = price_history.len() - graph_max_width();
+    graph_width = graph_max_width();
+  } else {
+    skip_amount = 0;
+    graph_width = price_history.len();
+  };
+  // Find the highest price over the given time period
+  let roof : Money = price_history.iter()
+                                  .skip( skip_amount )
+                                  // Find the highest value
+                                  .fold( 0., |mx, &vl| {
+                                    if vl > mx { vl }
+                                    else { mx }
+                                  } );
+  // Scale the step value to the highest price
+  let graph_step = roof / graph_height() as f64;
+  // Go through each line of the graph
+  for y in range( 0u, graph_height() ).rev() {
+    let price_point = y as f64 * graph_step;
+    // Our string buffer that holds a horizontal slice of the graph
+    let mut graph_slice = String::new();
+    for x in range( 0u, graph_width ) {
+      let x_price = price_history[skip_amount + x];
+      // Relative price to the current price point
+      let rel_price = (x_price - price_point + 1.) / graph_step;
+      // Build each cell if the right character that corresponds the it's value
+      graph_slice.push( if rel_price <= 0. {
+        ' '
+      } else if rel_price <= 0.25 {
+        '_'
+      } else if rel_price <= 0.50 {
+        '.'
+      } else if rel_price <= 0.75 {
+        '='
+      } else {
+        '#'
+      } );
+    }
+    // Print the axis and graph slice
+    println!( "{:9.2}|{}", price_point, graph_slice );
+  }
+  println!( "Time:    +{}>\n", String::from_char( graph_max_width(), '-' ) );
   // <GRAPH HERE>
   let assets = player.get_assets( &market.name );
   let total_gain = assets as f64 * market.price;
@@ -30,7 +108,6 @@ fn render_market_data( player : &Agent, market : &mut Market ) {
                                                           , market.name
                                                           , total_gain );
 }
-
 impl ConsoleInterface {
   pub fn new() -> ConsoleInterface {
       ConsoleInterface {
@@ -72,7 +149,7 @@ impl ConsoleInterface {
         println!( "" );
       }
     }
-    println!( "\nYour current funds: {} DKK", game.player.funds);
+    println!( "\nYour current funds: {:.2} DKK", game.player.funds);
     loop {
       let mut user_input = self.input.read_line().unwrap();
       user_input.pop();
@@ -94,7 +171,7 @@ impl ConsoleInterface {
                 },
                 _ => {
                   println!( "You buy {} {} for {:.2} DKK", x, name, x as f64 * buy_market.price);
-                  println!( "Your current funds: {} DKK", game.player.funds );
+                  println!( "Your current funds: {:.2} DKK", game.player.funds );
                 }
                } 
            } else {
@@ -109,11 +186,11 @@ impl ConsoleInterface {
             if let Some( x ) = from_str::<Count>( amount ) {
                match sell_market.sell_assets( &mut game.player, x ) {
                 Err( InsufficientAgentAssets ) => {
-                  println!( "You don't have enough assets of {} to sell", name);
+                  println!( "You don't have {} assets of {} to sell", x, name);
                 },
                 _ => {
-                  println!( "You sell {} {} for {:.2} DKK", x, name, x as f64 * sell_market.price);
-                  println!( "Your current funds: {} DKK", game.player.funds );
+                  println!( "You sell {} assets of {} for {:.2} DKK", x, name, x as f64 * sell_market.price);
+                  println!( "Your current funds: {:.2} DKK", game.player.funds );
                 }
                }
             } else {
